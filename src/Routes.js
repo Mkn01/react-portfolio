2bebe2f472bc9df918d2ed4f302a651ffa550f66
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-08-27 18:32:19 +0100

    linked home page through routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import { HashRouter, Route, Link, Routes } from "react-router-dom";
 import AboutMePage from "../src/components/aboutMe";
 import ResumePage from "./components/resume";
+import HomePage from "./components/home";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<div>Home</div>} />
+      <Route exact path="/" element={<HomePage />} />
       <Route exact path="/about-me" element={<AboutMePage />} />
       <Route exact path="/full-stack-projects" element={<div>Portfolio</div>} />
       <Route exact path="/frontend-projects" element={<div>Portfolio</div>} />

d5e62ce9ca9e131207e1ebfea26c73f6b4b97335
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-16 14:55:48 +0100

    added image

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,21 +1,35 @@
-import { Box, Paper } from "@mui/material";
+import { Avatar, Box, Paper } from "@mui/material";
 
 import { PageTitle } from "../components/header";
 
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { Button } from "@mui/material";
 import { Stack } from "@mui/system";
+import photo from "../components/assets/images/g.n.jpg";
 
 const HomePage = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
   return (
     <Box>
       <PageTitle title={"Welcome"} />
+
+      <Avatar
+        src={photo}
+        alt="Gurmanpreet Nagra"
+        sx={{
+          marginRight: isMobile ? 0 : 110,
+          marginLeft: isMobile ? 17 : 30,
+          marginTop: 3,
+          width: isMobile ? 89 : 400,
+          height: isMobile ? 90 : 400,
+        }}
+      />
       <Paper
         sx={{
-          marginLeft: isMobile ? 0 : 110,
+          marginLeft: isMobile ? 0 : 100,
           marginRight: isMobile ? 0 : 2,
-          marginTop: 2,
+          marginTop: -40,
+
           textAlign: "center",
         }}
       >
@@ -25,10 +39,11 @@ const HomePage = () => {
           my projects to see the work i've done so far.
         </h4>
       </Paper>
+
       <Stack
         sx={{
           marginLeft: isMobile ? 0 : 110,
-          marginTop: 2,
+          marginTop: 8,
           justifyContent: "space-around",
         }}
       >

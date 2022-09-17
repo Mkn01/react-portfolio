b5ead9c99f4c7cc11daf90e1931af4731c26b0f2
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-17 17:36:35 +0100

    fixing contact page

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -5,36 +5,46 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Icon } from "@mui/material";
 import { Padding } from "@mui/icons-material";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
 const Contact = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
   return (
     <div>
       <PageTitle title={"Contact"} />
-      <Box
-        backgroundColor="#c98986ff"
-        sx={{ marginLeft: 50, marginRight: 50, marginTop: 8, Padding: 10 }}
-      >
-        <Link
-          href="https://github.com/Mkn01"
-          target="_blank"
-          color="inherit"
-          sx={{
-            pr: 4,
-            "& :hover": { color: "#ffe26a" },
-          }}
-        >
-          <GitHubIcon fontSize="large" />
-        </Link>
-        <Link
-          href="https://www.linkedin.com/in/gurmanpreet-nagra/"
-          target="_blank"
-          color="inherit"
-          sx={{ "& :hover": { color: "#ffe26a" } }}
-        >
-          <LinkedInIcon fontSize="large" />
-        </Link>
-      </Box>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Box
+            backgroundColor="#c98986ff"
+            sx={{ marginLeft: 50, marginRight: 50, marginTop: 8, Padding: 10 }}
+          >
+            <Link
+              href="https://github.com/Mkn01"
+              target="_blank"
+              color="inherit"
+              sx={{
+                pr: 4,
+                "& :hover": { color: "#ffe26a" },
+              }}
+            >
+              <GitHubIcon fontSize="large" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/gurmanpreet-nagra/"
+              target="_blank"
+              color="inherit"
+              sx={{ "& :hover": { color: "#ffe26a" } }}
+            >
+              <LinkedInIcon fontSize="large" />
+            </Link>
+          </Box>
+        </CardContent>
+      </Card>
     </div>
   );
 };
